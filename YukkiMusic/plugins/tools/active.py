551ce4cdeac8f20da808bfc0bb1e4f98f71b802d
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2023-06-17 22:14:52 +0530

    Update active.py

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -21,7 +21,9 @@ ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
 ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
 
 
-@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
+@app.on_message(filters.command(ACTIVEVC_COMMAND) & filters.user(OWNER_ID)
+)
+@language
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
         "Getting active voice chats.. Please hold"
